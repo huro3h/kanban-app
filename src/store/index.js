2a8fbd5ab62113a04018c7f261d38e970996af14
huro3h <huro3h@gmail.com>
2021-07-25 19:00:37 +0900

    update index.js for Auth, Board state

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態 Auth と状態 BoardをVuexのstateで一元管理できるよう定義する
+const state = {
+  auth: { // 状態 Auth
+    token: null, // tokenはnullで初期化
+    userId: null // userIdもnullで初期化
+  },
+  board: { // 状態 Board
+    lists: [] // 状態 TaskList は空で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateを stateオプションに指定
   getters,
   actions,
   mutations,

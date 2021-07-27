59eaf5eda3d575026584db1c2d295cc0b4d94dc8
huro3h <huro3h@gmail.com>
2021-07-27 21:56:17 +0900

    update KbnButton.vue

--- src/components/atoms/KbnButton.vue
+++ src/components/atoms/KbnButton.vue
@@ -1,11 +1,35 @@
 <template>
-  <button>
-    KbnButton.vue
-    <slot />
+  <button
+    :class="classes"
+    type="button"
+  >
+    <slot/>
   </button>
 </template>
 
 <script>
+export default {
+  name: 'KbnButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  },
+
+  computed: {
+    // typeに応じてクラスを動的に生成する
+    classes () {
+      const cls = this.type === 'text' ? this.type : ''
+      return [`kbn-button-${cls}`]
+    }
+  },
+
+  methods: {
+    // TODO:
+  }
+}
 </script>
 
 <style scoped>

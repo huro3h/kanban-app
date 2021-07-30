d9a40384b575074f72368823ac8825ee895fd9a8
huro3h <huro3h@gmail.com>
2021-07-30 22:43:19 +0900

    update KbnLoginView

--- src/components/templates/KbnLoginView.vue
+++ src/components/templates/KbnLoginView.vue
@@ -1,9 +1,39 @@
 <template>
   <p>ログインページ</p>
 </template>
+<template>
+  <div class="login-view">
+    <h3>Kanban App</h3>
+    <KbnLoginForm :onlogin="handleLogin"/>
+  </div>
+</template>
 
 <script>
+import KbnLoginForm from '@/components/molecules/KbnLoginForm'
+
+export default {
+  name: 'KbnLoginView',
+
+  components: {
+    KbnLoginForm
+  },
+
+  methods: {
+    handleLogin (authInfo) {
+      return this.$store.dispatch('login', authInfo)
+        .then(() => {
+          this.$router.push({ path: '/' })
+        })
+        .catch(e => this.throwReject(e))
+    },
+    throwReject (err) { return Promise.reject(err) }
+  }
+}
 </script>
 
 <style scoped>
+.login-view {
+  width: 320px;
+  margin: auto;
+}
 </style>

ad974154fd9b08463946adab99cd110e20d37e07
huro3h <huro3h@gmail.com>
2021-08-02 22:05:41 +0900

    update guards.js

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,5 +1,13 @@
 import store from '../store'
 
 export const authorizeToken = (to, from, next) => {
-  // TODO
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.state.auth || !store.state.auth.token) {
+      next({ path: '/login' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 }
